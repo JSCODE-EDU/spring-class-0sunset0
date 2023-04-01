fc36cf596682200a13bd43b41a905ffaee7c20db
0sunset0 <chi6465618@naver.com>
2023-04-01 18:03:11 +0900

    Refactor: dto 반환 코드로 수정

--- spring-hello/src/main/java/com/springhello/domain/product/api/ProductJpaController.java
+++ spring-hello/src/main/java/com/springhello/domain/product/api/ProductJpaController.java
@@ -1,7 +1,9 @@
 package com.springhello.domain.product.api;
 
-import com.springhello.domain.product.dto.ProductResponse;
-import com.springhello.domain.product.dto.SaveProductRequest;
+import com.springhello.domain.product.dto.request.ProductSaveRequest;
+import com.springhello.domain.product.dto.response.ProductResponse;
+import com.springhello.domain.product.dto.response.ProductResult;
+import com.springhello.domain.product.dto.response.ProductSaveResponse;
 import com.springhello.domain.product.service.ProductJpaService;
 import com.springhello.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -16,25 +18,25 @@ public class ProductJpaController {
     private final ProductJpaService productService;
 
     @GetMapping("/products")
-    public List<ProductResponse> findAll() {
+    public ProductResult findAll() {
         return productService.findAll();
     }
 
     @PostMapping("/save")
-    public Long save(@RequestBody SaveProductRequest saveProductRequest) {
-        return productService.save(saveProductRequest);
+    public ProductSaveResponse save(@RequestBody ProductSaveRequest productSaveRequest) {
+        return productService.save(productSaveRequest);
     }
 
 
     //TODO 원화/달러 구분해서 보여주기 추가
     @GetMapping(value = "/product", params = "id")
     public ProductResponse findOneById(@RequestParam Long id, @RequestParam(required = false) String monetaryUnit) {
-        return productService.findOneById(id);
+        return productService.findOneById(id, monetaryUnit);
     }
 
     @GetMapping(value = "/product", params = "name")
     public ProductResponse findOneByName(@RequestParam String name, @RequestParam(required = false) String monetaryUnit) {
-        return productService.findOneByName(name);
+        return productService.findOneByName(name, monetaryUnit);
     }
 
 
--- spring-hello/src/main/java/com/springhello/domain/product/service/ProductJpaService.java
+++ spring-hello/src/main/java/com/springhello/domain/product/service/ProductJpaService.java
@@ -1,11 +1,17 @@
 package com.springhello.domain.product.service;
 
-import com.springhello.domain.product.dto.ProductResponse;
-import com.springhello.domain.product.dto.SaveProductRequest;
+
+import com.springhello.domain.product.dto.request.ProductSaveRequest;
+import com.springhello.domain.product.dto.response.ProductResponse;
+import com.springhello.domain.product.dto.response.ProductResult;
+import com.springhello.domain.product.dto.response.ProductSaveResponse;
+import com.springhello.domain.product.entity.MonetaryUnit;
 import com.springhello.domain.product.entity.Product;
 import com.springhello.domain.product.entity.ProductEntity;
 import com.springhello.domain.product.exception.DuplicateNameException;
+import com.springhello.domain.product.exception.ProductNotFoundException;
 import com.springhello.domain.product.repository.ProductJpaRepository;
+import com.springhello.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,38 +24,53 @@ import java.util.stream.Collectors;
 public class ProductJpaService {
     private final ProductJpaRepository productJpaRepository;
 
-    public List<ProductResponse> findAll(){
-        return productJpaRepository.findAll().stream()
+    //모든 상품 찾기
+    public ProductResult findAll() {
+        List<ProductResponse> productResponses = productJpaRepository.findAll().stream()
                 .map(p -> ProductResponse.from(p))
                 .collect(Collectors.toList());
+        return new ProductResult(productResponses);
     }
 
-    public Long save(SaveProductRequest saveProductRequest) {
-        checkProductDuplicate(saveProductRequest.getName());
-        ProductEntity productEntity = ProductEntity.createProduct(
-                saveProductRequest.getName(), saveProductRequest.getPrice());
-        ProductEntity saveProduct = productJpaRepository.save(productEntity);
-        return saveProduct.getId();
-
+    //상품 저장
+    public ProductSaveResponse save(ProductSaveRequest productSaveRequest) {
+        checkProductDuplicateName(productSaveRequest.getName());
+        ProductEntity saveProduct = ProductEntity.createProduct(productSaveRequest.getName(), productSaveRequest.getPrice());
+        Long saveId = productJpaRepository.save(saveProduct).getId();
+        return new ProductSaveResponse(saveId);
     }
 
-    private void checkProductDuplicate(String name) {
-        if (isExistedProduct(name)) {
+    //상품명 중복 검사
+    private void checkProductDuplicateName(String name) {
+        if (productJpaRepository.findByName(name).isPresent()) {
             throw new DuplicateNameException();
         }
     }
 
-    private boolean isExistedProduct(String name) {
-        return productJpaRepository.findByName(name).isPresent();
-    }
-
-    public ProductResponse findOneById(Long id) {
+    public ProductResponse findOneById(Long id, String monetaryUnit) {
         ProductEntity findProduct = productJpaRepository.findById(id).get();
-        return ProductResponse.from(findProduct);
+        ProductResponse productResponse = ProductResponse.from(findProduct);
+        return getProductResponse(monetaryUnit, productResponse);
     }
 
-    public ProductResponse findOneByName(String name) {
-        ProductEntity findProduct = productJpaRepository.findByName(name).get();
-        return ProductResponse.from(findProduct);
+    public ProductResponse findOneByName(String name, String monetaryUnit) {
+        // 없는 상품명으로 조회했을 때 조회 실패
+        ProductEntity findProduct = productJpaRepository.findByName(name)
+                .orElseThrow(() -> new ProductNotFoundException());
+        ProductResponse productResponse = ProductResponse.from(findProduct);
+        return getProductResponse(monetaryUnit, productResponse);
+    }
+
+
+    private ProductResponse getProductResponse(String monetaryUnit, ProductResponse productResponse) {
+        if (monetaryUnit != null && monetaryUnit.equals("dollar")){
+            productResponse.setMonetaryUnit(MonetaryUnit.DOLLAR);
+            productResponse.setPrice(changeToDollar(productResponse.getPrice()));
+        }
+        return productResponse;
+    }
+
+    private Long changeToDollar(Long won) {
+        return won/1300; //TODO 환율의 변화가 생겼을 때 변경하지 않아도 되게 하기
     }
 }
