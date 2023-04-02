f716e5666b2ab30b3929b2aa917869b3c9f9ece7
0sunset0 <chi6465618@naver.com>
2023-04-02 16:23:50 +0900

    Fix: 생성자 생성
    프록시 생성을 위한 기본 생성자 생성, 객체 생성을 위한 모든 매개변수가 있는 생성자 생성

--- spring-hello/src/main/java/com/springhello/domain/store/entity/Address.java
+++ spring-hello/src/main/java/com/springhello/domain/store/entity/Address.java
@@ -1,7 +1,6 @@
 package com.springhello.domain.store.entity;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -9,10 +8,13 @@ import javax.persistence.Embeddable;
 @Embeddable
 @Getter
 @EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
     @Column(name = "address_roadcode")
     private String roadCode;
     @Column(name = "address_detail")
     private String detail;
+
 }
