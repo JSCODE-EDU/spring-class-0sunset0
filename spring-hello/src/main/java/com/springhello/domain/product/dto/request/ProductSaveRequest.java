ed8d264717a78c12fffe3c67beb98a1a9682e1e2
0sunset0 <chi6465618@naver.com>
2023-04-02 16:20:32 +0900

    Feat: Dto에 필드 추가

--- spring-hello/src/main/java/com/springhello/domain/product/dto/request/ProductSaveRequest.java
+++ spring-hello/src/main/java/com/springhello/domain/product/dto/request/ProductSaveRequest.java
@@ -1,17 +1,23 @@
 package com.springhello.domain.product.dto.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter @Setter
+@NoArgsConstructor
 public class ProductSaveRequest {
 
-    @NotEmpty
+    @NotBlank
     private String name;
 
     @Range(max = 10000000)
     private Long price;
+
+    @NotNull
+    private Long storeId;
 }
