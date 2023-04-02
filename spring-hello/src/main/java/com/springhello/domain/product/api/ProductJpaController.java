56f86dd384d1232ef93f00e109faf3ccdc7ef0ae
0sunset0 <chi6465618@naver.com>
2023-04-02 16:26:12 +0900

    Feat: storeId로 products 조회하는 메서드 추가

--- spring-hello/src/main/java/com/springhello/domain/product/api/ProductJpaController.java
+++ spring-hello/src/main/java/com/springhello/domain/product/api/ProductJpaController.java
@@ -3,41 +3,48 @@ package com.springhello.domain.product.api;
 
 import com.springhello.domain.product.dto.request.ProductSaveRequest;
 import com.springhello.domain.product.dto.response.ProductResponse;
+import com.springhello.domain.product.dto.response.ProductResult;
 import com.springhello.domain.product.dto.response.ProductSaveResponse;
 import com.springhello.domain.product.service.ProductJpaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.validation.Valid;
 
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/product")
 public class ProductJpaController {
 
     private final ProductJpaService productJpaService;
 
-    @GetMapping("/products")
-    public List<ProductResponse> findAll() {
+    @GetMapping("/all")
+    public ProductResult findAll() {
         return productJpaService.findAll();
     }
 
+    @GetMapping(value = "/all", params = "storeId")
+    public ProductResult findAllByStoreId(@RequestParam Long storeId) {
+        return productJpaService.findAllByStoreId(storeId);
+    }
+
     @PostMapping("/save")
-    public Long save(@RequestBody ProductSaveRequest productSaveRequest) {
+    public ProductSaveResponse save(@Valid @RequestBody ProductSaveRequest productSaveRequest, BindingResult bindingResult) {
         return productJpaService.save(productSaveRequest);
     }
 
 
     //TODO 원화/달러 구분해서 보여주기 추가
-    @GetMapping(value = "/product", params = "id")
+    @GetMapping(value = "/detail", params = "id")
     public ProductResponse findOneById(@RequestParam Long id, @RequestParam(required = false) String monetaryUnit) {
         return productJpaService.findOneById(id);
     }
 
-    @GetMapping(value = "/product", params = "name")
+    @GetMapping(value = "/detail", params = "name")
     public ProductResponse findOneByName(@RequestParam String name, @RequestParam(required = false) String monetaryUnit) {
         return productJpaService.findOneByName(name);
     }
-
 
 }
--- spring-hello/src/main/java/com/springhello/domain/product/repository/ProductJpaRepository.java
+++ spring-hello/src/main/java/com/springhello/domain/product/repository/ProductJpaRepository.java
@@ -4,10 +4,13 @@ import com.springhello.domain.product.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductJpaRepository extends JpaRepository<ProductEntity, Long> {
 
     Optional<ProductEntity> findByName(String name);
+
+    List<ProductEntity> findAllByStoreId(Long id);
 
 }
