df2425a1e56a0521376839a44af18653acd31bfe
0sunset0 <chi6465618@naver.com>
2023-04-02 16:22:14 +0900

    Refactor: List나 id가 아닌 Dto Response를 반환하게 변경

--- spring-hello/src/main/java/com/springhello/domain/product/service/ProductJpaService.java
+++ spring-hello/src/main/java/com/springhello/domain/product/service/ProductJpaService.java
@@ -3,10 +3,15 @@ package com.springhello.domain.product.service;
 
 import com.springhello.domain.product.dto.request.ProductSaveRequest;
 import com.springhello.domain.product.dto.response.ProductResponse;
+import com.springhello.domain.product.dto.response.ProductResult;
 import com.springhello.domain.product.dto.response.ProductSaveResponse;
 import com.springhello.domain.product.entity.ProductEntity;
-import com.springhello.domain.product.exception.DuplicateNameException;
+import com.springhello.domain.product.exception.DuplicateProductNameException;
+import com.springhello.domain.product.exception.ProductNotFoundException;
 import com.springhello.domain.product.repository.ProductJpaRepository;
+import com.springhello.domain.store.entity.Store;
+import com.springhello.domain.store.exception.StoreNotFoundException;
+import com.springhello.domain.store.repository.StoreJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,40 +23,50 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ProductJpaService {
     private final ProductJpaRepository productJpaRepository;
+    private final StoreJpaRepository storeJpaRepository;
 
-    public List<ProductResponse> findAll(){
-        return productJpaRepository.findAll().stream()
-                .map(p -> ProductResponse.from(p))
+    public ProductResult findAll(){
+        List<ProductResponse> productResponse = productJpaRepository.findAll().stream()
+                .map(product -> ProductResponse.from(product))
                 .collect(Collectors.toList());
+        return ProductResult.from(productResponse);
     }
 
-    //TODO
-    public Long save(ProductSaveRequest productSaveRequest) {
+    public ProductSaveResponse save(ProductSaveRequest productSaveRequest) {
         checkProductDuplicate(productSaveRequest.getName());
+        Store findStore = storeJpaRepository.findById(productSaveRequest.getStoreId())
+                .orElseThrow(() -> new StoreNotFoundException());
+
         ProductEntity productEntity = ProductEntity.createProduct(
-                productSaveRequest.getName(), productSaveRequest.getPrice());
+                productSaveRequest.getName(), productSaveRequest.getPrice(), findStore);
         ProductEntity saveProduct = productJpaRepository.save(productEntity);
-        return saveProduct.getId();
+        return ProductSaveResponse.from(saveProduct);
 
     }
 
     private void checkProductDuplicate(String name) {
-        if (isExistedProduct(name)) {
-            throw new DuplicateNameException();
+        if (productJpaRepository.findByName(name).isPresent()) {
+            throw new DuplicateProductNameException();
         }
     }
 
-    private boolean isExistedProduct(String name) {
-        return productJpaRepository.findByName(name).isPresent();
-    }
-
     public ProductResponse findOneById(Long id) {
-        ProductEntity findProduct = productJpaRepository.findById(id).get();
+        ProductEntity findProduct = productJpaRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException());
         return ProductResponse.from(findProduct);
     }
 
     public ProductResponse findOneByName(String name) {
-        ProductEntity findProduct = productJpaRepository.findByName(name).get();
+        ProductEntity findProduct = productJpaRepository.findByName(name)
+                .orElseThrow(() -> new ProductNotFoundException());
         return ProductResponse.from(findProduct);
+    }
+
+    //하나의 상점에 속하는 모든 상품 조회
+    public ProductResult findAllByStoreId(Long storeId) {
+        List<ProductResponse> productResponses = productJpaRepository.findAllByStoreId(storeId).stream()
+                .map(productEntity -> ProductResponse.from(productEntity))
+                .collect(Collectors.toList());
+        return ProductResult.from(productResponses);
     }
 }
