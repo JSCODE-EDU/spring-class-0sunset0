756ed630b9fd8a084da63528493797e244f45fcf
0sunset0 <chi6465618@naver.com>
2023-04-02 13:26:42 +0900

    Style: 서비스 주입 필드 이름 변경

--- spring-hello/src/main/java/com/springhello/domain/product/api/ProductJpaController.java
+++ spring-hello/src/main/java/com/springhello/domain/product/api/ProductJpaController.java
@@ -15,28 +15,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProductJpaController {
 
-    private final ProductJpaService productService;
+    private final ProductJpaService productJpaService;
 
     @GetMapping("/products")
     public ProductResult findAll() {
-        return productService.findAll();
+        return productJpaService.findAll();
     }
 
     @PostMapping("/save")
     public ProductSaveResponse save(@RequestBody ProductSaveRequest productSaveRequest) {
-        return productService.save(productSaveRequest);
+        return productJpaService.save(productSaveRequest);
     }
 
 
     //TODO 원화/달러 구분해서 보여주기 추가
     @GetMapping(value = "/product", params = "id")
     public ProductResponse findOneById(@RequestParam Long id, @RequestParam(required = false) String monetaryUnit) {
-        return productService.findOneById(id, monetaryUnit);
+        return productJpaService.findOneById(id, monetaryUnit);
     }
 
     @GetMapping(value = "/product", params = "name")
     public ProductResponse findOneByName(@RequestParam String name, @RequestParam(required = false) String monetaryUnit) {
-        return productService.findOneByName(name, monetaryUnit);
+        return productJpaService.findOneByName(name, monetaryUnit);
     }
 
 
