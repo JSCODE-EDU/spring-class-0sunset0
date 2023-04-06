25d8349a1ef83c2bfd88c8704e1023394cf84c17
0sunset0 <chi6465618@naver.com>
2023-04-06 16:30:18 +0900

    Refactor: 빌더로 변경

--- spring-hello/src/main/java/com/springhello/domain/store/dto/response/StoreResponse.java
+++ spring-hello/src/main/java/com/springhello/domain/store/dto/response/StoreResponse.java
@@ -3,23 +3,34 @@ package com.springhello.domain.store.dto.response;
 
 import com.springhello.domain.store.entity.Store;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
 public class StoreResponse {
 
     private String name;
-    private String roadCode;
-    private String detail;
-    private String phoneValue;
+    private String addressRoadCode;
+    private String addressDetail;
+    private String phoneNumber;
 
     public static StoreResponse from(Store store) {
-        return new StoreResponse(store.getName(),
-                store.getAddress().getRoadCode(), store.getAddress().getDetail(),
-                store.getPhone().getValue());
+        return StoreResponse.builder()
+                .name(store.getName())
+                .addressRoadCode(store.getAddress().getRoadCode())
+                .addressDetail(store.getAddress().getRoadCode())
+                .phoneNumber(store.getPhone().getValue())
+                .build();
+    }
+
+    @Builder
+    private StoreResponse(String name, String addressRoadCode, String addressDetail, String phoneNumber) {
+        this.name = name;
+        this.addressRoadCode = addressRoadCode;
+        this.addressDetail = addressDetail;
+        this.phoneNumber = phoneNumber;
     }
 }
