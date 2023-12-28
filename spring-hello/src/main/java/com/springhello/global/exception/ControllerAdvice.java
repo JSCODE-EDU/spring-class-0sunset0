4465fe09bb4937dcac7ed6f743922bbd47331a48
0sunset0 <chi6465618@naver.com>
2023-12-28 21:35:47 +0900

    refactor: 4회차 미션

--- spring-hello/src/main/java/com/springhello/global/exception/ControllerAdvice.java
+++ spring-hello/src/main/java/com/springhello/global/exception/ControllerAdvice.java
@@ -1,4 +1,14 @@
 package com.springhello.global.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ControllerAdvice {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> IllegalArgumentException(RuntimeException exception) {
+        return null;
+    }
 }
--- spring-hello/src/main/java/com/springhello/global/exception/ErrorResponse.java
+++ spring-hello/src/main/java/com/springhello/global/exception/ErrorResponse.java
@@ -1,4 +1,9 @@
 package com.springhello.global.exception;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class ErrorResponse {
+    private Integer status;
+    private String message;
 }
